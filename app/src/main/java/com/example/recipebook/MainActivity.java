5553bc99d04c81e307533cce438d48c2e7aeb24f
Ravenrex10 <lucas.colbert@hotmail.com>
2023-12-20 00:31:47 +0100

    Listar recetas funciona

--- app/src/main/java/com/example/recipebook/MainActivity.java
+++ app/src/main/java/com/example/recipebook/MainActivity.java
@@ -3,9 +3,15 @@ package com.example.recipebook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     private RecipeDbHelper dbHelper;
     private SQLiteDatabase db;
-
+    private LinearLayout recipeContainer;
     private List<Recipe> recipeList;
 
     @Override
@@ -23,10 +29,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        recipeContainer = findViewById(R.id.recipeContainer);
+
         dbHelper = new RecipeDbHelper(getApplicationContext(), "recipe.db");
         db = dbHelper.getWritableDatabase();
 
-        listRecipes();  //Listar todas las recetas al abrir la aplicación.
+        listRecipes();  // Listar todas las recetas.
     }
 
 
@@ -35,11 +43,37 @@ public class MainActivity extends AppCompatActivity {
 
         for(Recipe recipe : recipeList){
 
+            View recipeCardView = LayoutInflater.from(this).inflate(R.layout.recipe_card, null);
+
+            // Views para el "card" del frontend
+            TextView textRecipeName = recipeCardView.findViewById(R.id.textRecipeName);
+            TextView textIngredients = recipeCardView.findViewById(R.id.textIngredients);
+            TextView textSteps = recipeCardView.findViewById(R.id.textSteps);
+            TextView textTime = recipeCardView.findViewById(R.id.textTime);
+
+            textRecipeName.setText(getString(R.string.recipeName) + ": " + recipe.getNombre());
+            textIngredients.setText(getString(R.string.ingredients) + ": " + recipe.getIngredientes());
+            textSteps.setText(getString(R.string.steps) + ": " + recipe.getPasos());
+            textTime.setText(getString(R.string.time) + " " + recipe.getTiempo() + " " + getString(R.string.minutes));
+
+            // Add the card to the container
+            recipeContainer.addView(recipeCardView);
         }
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.createRecipe: createRecipe(); break;
+        }
+    }
+
+    private void createRecipe() {
+
     }
 
     @SuppressLint("Range")
     private List<Recipe> getAllRecipes(){
+        List<Recipe> recipeList = new ArrayList<>();
         String[] columns = {
                 RecipeContract.RecipeEntry._ID,
                 RecipeContract.RecipeEntry.COLUMN_NAME_NOMBRE,
@@ -53,11 +87,10 @@ public class MainActivity extends AppCompatActivity {
         String ingredienteString = "";
         List<String> ingredientes = new ArrayList<String>();
         String pasos = "";
-        Double tiempo = -1.0;
+        Double tiempo = 0.0;
 
         try {
             while (cursor.moveToNext()) {
-
                 nombre = cursor.getString(cursor.getColumnIndex(RecipeContract.RecipeEntry.COLUMN_NAME_NOMBRE));
                 ingredienteString = cursor.getString(cursor.getColumnIndex(RecipeContract.RecipeEntry.COLUMN_NAME_INGREDIENTES));
                 ingredientes = Arrays.asList(ingredienteString.split(","));
@@ -70,6 +103,7 @@ public class MainActivity extends AppCompatActivity {
         } finally {
             cursor.close();
         }
+
         return recipeList;
     }
 }
